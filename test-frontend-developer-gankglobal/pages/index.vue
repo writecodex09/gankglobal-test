2fd3b6f36889c19e23351df2821c0bf1291f440d
writecodex09 <writecodex09@gmail.com>
2020-06-24 00:35:08 -0700

    first commit

---
+++ test-frontend-developer-gankglobal/pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+<section class="hero">
+  <div class="hero-body">
+    <div class="container">
+      <h1 class="title">
+        Coctailspedia
+      </h1>
+      <h2 class="subtitle">
+        This project using Nuxt.JS
+      </h2>
+    </div>
+  </div>
+</section>
+<div class="panel">
+  <div class="panel-heading">
+    <div class="panel-block" v-for="coctail in coctails" :key="coctail.strDrink">
+    {{coctails.drinks}}
+    </div>
+  </div>
+</div>
+  </div>
+</template>
+
+<script>
+import axios from '~/plugins/axios'
+
+export default{
+  asyncData(){
+      return axios.get('coctails')
+      .then(res => ({
+        coctails : res.data
+      }))
+  }
+}
+</script>
+
